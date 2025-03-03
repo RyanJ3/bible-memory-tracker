0867d5c78856b2f3bcb867b704eb4a4d70cc2af6
rmcelhaney <ryanmcelhaney@mac.com>
2025-03-02 21:12:15 -0500

    removing misleading comment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import {BibleTrackerComponent} from './bible-tracker/bible-tracker.component';
 
 export const routes: Routes = [
   { path: '', component: BibleTrackerComponent },  // Home page
-  { path: 'stats', component: BibleTrackerComponent },  // Bible dropdown route
+  { path: 'stats', component: BibleTrackerComponent }, // stats page
   { path: 'flow-memorization', component: FlowMemorizationComponent },  // FLOW memorization tool route
   { path: '**', redirectTo: '' }  // Catch all other routes and redirect to home
 ];
