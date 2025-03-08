208c04c3275161fa6946bac13b5a395cdae6908e
RyanJ3 <33846691+RyanJ3@users.noreply.github.com>
2025-03-07 21:36:52 -0500

    Add files via upload
    styling enhancements

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Amplify } from 'aws-amplify';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+// In main.ts or dedicated config file
+
+// Amplify.configure({
+//   Auth: {
+//     region: 'YOUR_REGION',             // e.g., 'us-east-1'
+//     userPoolId: 'YOUR_USER_POOL_ID',
+//     userPoolWebClientId: 'YOUR_APP_CLIENT_ID'
+//   }
+// });
